69471b9cdfc8b3a0e1e6e99fb0ca92c749a7685f
Nguyễn Anh Tuấn <50624746+Clarence161095@users.noreply.github.com>
2024-10-02 18:06:54 +0900

    update sh

--- remove-specific-resources.sh
+++ remove-specific-resources.sh
@@ -58,20 +58,45 @@ aws ec2 describe-instances --filters "Name=tag:Name,Values=$PREFIX*" | jq -r '.R
 done
 
 # Roles
-aws iam list-roles | jq -r '.Roles[] | select(.RoleName | startswith("'"$PREFIX"'")) | .RoleName' | while read x; do
-    aws iam delete-role --role-name $x
+aws iam list-roles | jq -r '.Roles[] | select(.RoleName | startswith("'"$PREFIX"'")) | .RoleName' | while read role; do
+    # Detach all managed policies
+    aws iam list-attached-role-policies --role-name $role | jq -r '.AttachedPolicies[].PolicyArn' | while read policy_arn; do
+        aws iam detach-role-policy --role-name $role --policy-arn $policy_arn
+    done
+    
+    # Delete all inline policies
+    aws iam list-role-policies --role-name $role | jq -r '.PolicyNames[]' | while read policy_name; do
+        aws iam delete-role-policy --role-name $role --policy-name $policy_name
+    done
+    
+    # Delete the role
+    aws iam delete-role --role-name $role
 done
 
 # Policies
-aws iam list-policies | jq -r '.Policies[] | select(.PolicyName | startswith("'"$PREFIX"'")) | .PolicyName' | while read x; do
-    aws iam delete-policy --policy-arn $x
+aws iam list-policies --scope Local | jq -r '.Policies[] | select(.PolicyName | startswith("'"$PREFIX"'")) | .Arn' | while read policy_arn; do
+    # Detach policy from all entities
+    aws iam list-entities-for-policy --policy-arn $policy_arn | jq -r '.PolicyRoles[].RoleName' | while read role; do
+        aws iam detach-role-policy --role-name $role --policy-arn $policy_arn
+    done
+    
+    # Delete all versions except the default version
+    aws iam list-policy-versions --policy-arn $policy_arn | jq -r '.Versions[] | select(.IsDefaultVersion==false) | .VersionId' | while read version; do
+        aws iam delete-policy-version --policy-arn $policy_arn --version-id $version
+    done
+    
+    # Delete the policy
+    aws iam delete-policy --policy-arn $policy_arn
 done
 
 # S3 - Remember alway remove S3 after all
 aws s3api list-buckets --query "Buckets[?starts_with(Name, '$PREFIX')].Name" --output text | tr '\t' '\n' | while read bucket; do
-  echo "bucket is deleting: $bucket"
+  # Delete all objects from the bucket
   aws s3api delete-objects --bucket "$bucket" --delete "$(aws s3api list-object-versions --bucket "$bucket" --output=json --query='{Objects: Versions[].{Key:Key,VersionId:VersionId}}')"
+  
+  # Delete all object delete markers from the bucket
   aws s3api delete-objects --bucket "$bucket" --delete "$(aws s3api list-object-versions --bucket "$bucket" --output=json --query='{Objects: DeleteMarkers[].{Key:Key,VersionId:VersionId}}')"
+  
+  # Delete the bucket
   aws s3 rb "s3://$bucket" --force
-  echo "bucket is deleted: $bucket"
 done
